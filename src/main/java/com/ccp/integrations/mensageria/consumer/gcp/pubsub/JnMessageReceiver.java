c0aa7dea48f64eee45c280da6eabe5f9f7205451
onias <onias@noxxonsat.com.br>
2022-12-30 21:38:22 -0300

    Implementação de async business

--- src/main/java/com/ccp/integrations/mensageria/consumer/gcp/pubsub/JnMessageReceiver.java
+++ src/main/java/com/ccp/integrations/mensageria/consumer/gcp/pubsub/JnMessageReceiver.java
@@ -1,7 +1,8 @@
 package com.ccp.integrations.mensageria.consumer.gcp.pubsub;
 
 import com.ccp.decorators.CcpMapDecorator;
-import com.ccp.dependency.injection.CcpSpecification;
+import com.ccp.dependency.injection.CcpDependencyInject;
+import com.ccp.especifications.instant.messenger.CcpInstantMessenger;
 import com.ccp.process.CcpProcess;
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 import com.google.cloud.pubsub.v1.MessageReceiver;
@@ -10,16 +11,19 @@ import com.google.pubsub.v1.PubsubMessage;
 
 public class JnMessageReceiver implements MessageReceiver {
 	
-	@CcpSpecification
-	private CcpProcess process;
+	@CcpDependencyInject
+	private final CcpProcess process;
 	
-	public JnMessageReceiver(CcpProcess process) {
+	@CcpDependencyInject
+	private CcpInstantMessenger instantMessenger;
+
+	private CcpMapDecorator parameters;
+
+	public JnMessageReceiver(CcpProcess process, CcpMapDecorator parameters) {
+		this.parameters = parameters;
 		this.process = process;
 	}
 
-
-
-	@Override
 	public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
 		try {
 			ByteString data = message.getData();
@@ -28,6 +32,7 @@ public class JnMessageReceiver implements MessageReceiver {
 			this.process.execute(mdMessage);
 			consumer.ack();
 		} catch (Exception e) {
+			this.instantMessenger.sendErrorToSupport(this.parameters, e);
 			consumer.nack();
 		}
 		
--- src/main/java/com/ccp/integrations/mensageria/consumer/gcp/pubsub/PubSubStarter.java
+++ src/main/java/com/ccp/integrations/mensageria/consumer/gcp/pubsub/PubSubStarter.java
@@ -4,11 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 
 import com.ccp.decorators.CcpMapDecorator;
+import com.ccp.dependency.injection.CcpDependencyInject;
 import com.ccp.dependency.injection.CcpDependencyInjection;
-import com.ccp.dependency.injection.CcpSpecification;
 import com.ccp.especifications.instant.messenger.CcpInstantMessenger;
 import com.ccp.implementations.instant.messages.telegram.InstantMessenger;
 import com.ccp.jn.sync.AsyncServices;
@@ -23,15 +22,20 @@ import com.google.cloud.pubsub.v1.Subscriber.Builder;
 import com.google.pubsub.v1.ProjectSubscriptionName;
 
 public class PubSubStarter {
-	final CcpMapDecorator args;
-	@CcpSpecification
+
+	final CcpMapDecorator parameters;
+	
+	@CcpDependencyInject
 	CcpInstantMessenger instantMessenger;
+	
+	@CcpDependencyInject
 	private final MessageReceiver queue;
+
 	public PubSubStarter( CcpMapDecorator args) {
-		this.args = args;
-		String topic = this.args.getAsString("topic");
+		this.parameters = args;
+		String topic = this.parameters.getAsString("topic");
 		CcpProcess process = AsyncServices.catalog.getAsObject(topic);
-		this.queue = new JnMessageReceiver(process);
+		this.queue = new JnMessageReceiver(process, this.parameters);
 
 	}
 		
@@ -39,9 +43,9 @@ public class PubSubStarter {
 		
 		Subscriber subscriber = null;
 		try {
-			String topic = this.args.getAsString("topic");
-			String projectName = this.args.getAsString("project");
-			Integer threads = this.args.getAsIntegerNumber("threads");
+			String topic = this.parameters.getAsString("topic");
+			String projectName = this.parameters.getAsString("project");
+			Integer threads = this.parameters.getAsIntegerNumber("threads");
 			
 			ProjectSubscriptionName subscription = ProjectSubscriptionName.of(projectName, topic);
 			ExecutorProvider executorProvider = InstantiatingExecutorProvider.newBuilder().setExecutorThreadCount(threads).build();
@@ -55,7 +59,7 @@ public class PubSubStarter {
 			subscriber.startAsync();
 			subscriber.awaitTerminated();
 		} catch (Throwable e) {
-			this.notificarErro(e);
+			this.instantMessenger.sendErrorToSupport(this.parameters, e);
 		} finally {
 			if (subscriber != null) {
 				subscriber.stopAsync();
@@ -63,15 +67,9 @@ public class PubSubStarter {
 		}
 	}
 
-	private void notificarErro(Throwable e) {
-		String supportId = this.args.getAsString("supportId");
-		String asJson = new CcpMapDecorator(e).asJson();
-		this.instantMessenger.sendMessageToSupport(supportId, asJson);
-	}
-
 	private FixedCredentialsProvider getCredentials(){
 		
-		String fileName = this.args.getAsString("credentials");
+		String fileName = this.parameters.getAsString("credentials");
 		File file = new File(fileName);
 		
 		try(InputStream credentials = new FileInputStream(file)){
@@ -79,19 +77,18 @@ public class PubSubStarter {
 			return create;
 			
 		}catch(IOException e) {
-			this.notificarErro(e);
+			this.instantMessenger.sendErrorToSupport(this.parameters, e);
 			throw new RuntimeException(e);
 		}
 	}
 
 	
 	public static void main(String[] args) {
-		CcpDependencyInjection.loadAllInstances(new InstantMessenger());
+		CcpDependencyInjection.loadAllImplementationsProviders(new InstantMessenger());
 		String json = args[0];
 		CcpMapDecorator md = new CcpMapDecorator(json);
 		PubSubStarter instance = new PubSubStarter(md);
 		CcpDependencyInjection.injectDependencies(instance);
-
 	}
 	
 }
